6a424b141dadc5b27b6aa9d1a4ba7f782d4442b1
umamatov <u.mamatov1502@gmail.com>
2022-04-12 14:40:05 +0500

    Initial Commit

---
+++ NextApelsinProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NextApelsinProject
+//
+//  Created by Umidjon Mamatov on 12/04/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
